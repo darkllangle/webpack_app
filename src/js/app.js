6b5bf1be55f932e73be5ab01ca8ef184a1639737
darkllangle <darkllangle@yahoo.com>
2017-05-17 05:14:30 +0800

    Since the "value" is available, then better using it

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ let people = require('./people.js');
 let $ = require('jquery');
 
 $.each(people, function(key, value){
-  $('body').append('<h1>'+people[key].name+'</h1>');
+  $('body').append('<h1>'+value.name+'</h1>');
 });
 
 console.log(people[0].name);
